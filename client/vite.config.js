2a521654a64b18255d1913511257a1724956e1f3
codessphere <codessphereinfo@gmail.com>
2025-11-06 14:49:45 +0530

    update to file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-}) 
+  plugins: [react()],
+  root: '.',          // client folder root
+  build: {
+    outDir: 'dist',   // build folder
+    emptyOutDir: true
+  }
+})
